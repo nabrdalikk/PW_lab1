c41fbaaacd448671680a4a0f06a443b3cd34cdbd
Student <student@ib.polsl.pl>
2017-02-27 11:35:51 +0100

    commit 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+#include "LaborkaConfig.h"
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include "trygonometria.h"
+#else
+#include "math.h"
+#endif
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+result = cos_degree(45.0);
+#else
+result = cos(pi/4.0);
+#endif
+
 int main()
 {
 return 0;
